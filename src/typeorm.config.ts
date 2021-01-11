3d3096f18fd07601f701197f8c65cc5867b22c78
TusharRoy23 <risenboy23@gmail.com>
2021-01-11 13:24:50 +0600

    update and bug fixing

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as config from 'config'
+
+const dbConfig = config.get('db')
 
 const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: process.env.POSTGRES_HOST || 'postgres-db',
+    type: process.env.DB_TYPE || dbConfig.type,
+    host: process.env.POSTGRES_HOST || dbConfig.host,
     port: +process.env.POSTGRES_PORT || 5432,
-    username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.POSTGRES_USER || 'postgres',
-    database: process.env.POSTGRES_DB || 'tododb',
+    username: process.env.DB_USERNAME || dbConfig.username,
+    password: process.env.DB_PASSWORD || dbConfig.password,
+    database: process.env.POSTGRES_DB || dbConfig.database,
     entities: [__dirname + '/**/*.entity.ts', __dirname + '/**/*.entity.js'],
     migrationsRun: false,
     logging: true,
